a515b26a12382e3c401e2776febadbaaf1c7c933
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-25 14:30:09 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-25

--- app/Lizerium.Restarter.Server/Services/ApiPoint.cs
+++ app/Lizerium.Restarter.Server/Services/ApiPoint.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 июля 2026 11:38:53
- * Version: 1.0.96
+ * Last Updated: 25 июля 2026 14:30:08
+ * Version: 1.0.97
  */
 
 using System.Text;
