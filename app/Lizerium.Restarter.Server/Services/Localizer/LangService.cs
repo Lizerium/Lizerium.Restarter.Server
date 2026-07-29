19c900e10aac604c11fb57a868c7c8f99dbf510f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-29 15:45:41 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-29

--- app/Lizerium.Restarter.Server/Services/Localizer/LangService.cs
+++ app/Lizerium.Restarter.Server/Services/Localizer/LangService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 июля 2026 10:10:46
- * Version: 1.0.100
+ * Last Updated: 29 июля 2026 15:45:40
+ * Version: 1.0.101
  */
 
 namespace Lizerium.Restarter.Server.Services.Localizer
