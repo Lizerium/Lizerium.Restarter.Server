a11a6ddd0ab585ecd3a76af5da2e6ef4ffea73ef
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-13 06:52:55 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-13

--- app/Lizerium.Restarter.Server/Components/Config.cs
+++ app/Lizerium.Restarter.Server/Components/Config.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 августа 2026 06:53:04
- * Version: 1.0.115
+ * Last Updated: 13 августа 2026 06:52:54
+ * Version: 1.0.116
  */
 
 using System.Text.Json.Serialization;
